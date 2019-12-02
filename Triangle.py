f8b407742942849d1517970aba1fb43a0b9787ba
JF Marronnier <contact@epitez.fr>
2019-12-02 23:07:59 +0100

    [R] add method _is_equilateral

--- Triangle.py
+++ Triangle.py
@@ -2,6 +2,9 @@ class Triangle:
     def __init__(self, a, b, c):
         self._a, self._b, self._c = sorted([a, c, b])
 
+    def _is_equilateral__(self):
+        return self._a == self._b and self._b == self._c
+
     def __str__(self):
-        sufix = " is equilateral" if self._a == self._b and self._b == self._c else ""
+        sufix = " is equilateral" if self._is_equilateral__() else ""
         return "Triangle sides length: %d, %d, %d%s" % (self._a, self._b, self._c, sufix)
