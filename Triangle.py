637c3556cd06f02bf7fcbf0fd74d274d0f07e286
JF Marronnier <contact@epitez.fr>
2019-12-02 23:24:42 +0100

    [R] renam field

--- Triangle.py
+++ Triangle.py
@@ -1,13 +1,13 @@
 class Triangle:
     def __init__(self, a, b, c):
-        self._a, self._b, self._c = sorted([a, c, b])
+        self._shortest_side, self._middle_side, self._longest_side = sorted([a, c, b])
 
     def _is_equilateral__(self):
-        return self._a == self._b and self._b == self._c
+        return self._shortest_side == self._middle_side and self._middle_side == self._longest_side
 
     def _is_isosceles__(self):
-        return self._a == self._b or self._b == self._c
+        return self._shortest_side == self._middle_side or self._middle_side == self._longest_side
 
     def __str__(self):
         sufix = " is equilateral" if self._is_equilateral__() else " is isosceles" if self._is_isosceles__() else ""
-        return "Triangle sides length: %d, %d, %d%s" % (self._a, self._b, self._c, sufix)
+        return "Triangle sides length: %d, %d, %d%s" % (self._shortest_side, self._middle_side, self._longest_side, sufix)
