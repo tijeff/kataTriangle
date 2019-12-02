d0d99adebd66622d4db84b9489aaac5e3d0bcf7f
JF Marronnier <contact@epitez.fr>
2019-12-02 23:02:33 +0100

    [R] sort side

--- Triangle.py
+++ Triangle.py
@@ -1,6 +1,6 @@
 class Triangle:
     def __init__(self, a, b, c):
-        self._a, self._b, self._c = (a, c, b)
+        self._a, self._b, self._c = sorted([a, c, b])
 
     def __str__(self):
         return "Triangle sides length: %d, %d, %d" % (self._a, self._b, self._c)
