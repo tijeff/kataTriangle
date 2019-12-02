a1d5146bfc6de7d7e7a89ab73fc24d9b68ba2d75
JF Marronnier <contact@epitez.fr>
2019-12-02 23:22:52 +0100

    [R] add _is_isosceles method

--- Triangle.py
+++ Triangle.py
@@ -5,6 +5,9 @@ class Triangle:
     def _is_equilateral__(self):
         return self._a == self._b and self._b == self._c
 
+    def _is_isosceles__(self):
+        return self._a == self._b or self._b == self._c
+
     def __str__(self):
-        sufix = " is equilateral" if self._is_equilateral__() else " is isosceles" if self._a == self._b or self._b == self._c else ""
+        sufix = " is equilateral" if self._is_equilateral__() else " is isosceles" if self._is_isosceles__() else ""
         return "Triangle sides length: %d, %d, %d%s" % (self._a, self._b, self._c, sufix)
